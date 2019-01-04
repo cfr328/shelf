6f3887e52a6ea1fb6e5513578fe584c8fe30a80a
cfr <1771952134@qq.com>
2019-01-04 08:30:38 +0800

    chelun2

---
+++ count/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <button @click="login">登陆</button>
+</template>
+
+<script>
+import JSBridge from './utils/JSBridge.js';
+export default {
+  name: 'app',
+  data () {
+    return {
+    }
+  },
+  methods: {
+    login(){
+      //登录
+      JSBridge.invoke('app', 'login1000000000', {
+        loginCallBackName: res=>{
+          console.log('res...', res);
+        },
+        a: 1,
+        b: 2,
+        c: 3
+      });
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
